016196f73a9d5a75c8d863cb797835206e054c22
sdavis <sdavis@d2ad0ced-163c-4466-90a1-0dc60eb45a10>
2008-09-23 13:36:35 +0000

    Displace tiene nueva opcion "autocenter", que centra los atomos en la celda de simulacion

--- plugins/testing/displace.cc
+++ plugins/testing/displace.cc
@@ -17,6 +17,7 @@ DisplaceModifier::DisplaceModifier(std::string args): Module("displace")
  AssignParameter("x", "1.0");
  AssignParameter("y", "0.0");
  AssignParameter("z", "0.0");
+ AssignParameter("autocenter", "false");
  // 
  ProcessArguments(args);
  offset = Vector(GetDouble("x"), GetDouble("y"), GetDouble("z"));
@@ -40,6 +41,8 @@ void DisplaceModifier::ShowHelp() const
  std::cout << "      x             : Coord. X del vector de desplazamiento                    \n";
  std::cout << "      y             : Coord. Y del vector de desplazamiento                    \n";
  std::cout << "      z             : Coord. Z del vector de desplazamiento                    \n";
+ std::cout << "      autocenter    : Si es true se escoge x, y, z automaticamente para centrar\n";
+ std::cout << "                      los atomos en la celda de simulacion (default=false)     \n";
  std::cout << "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n";
  std::cout << " Example                                                                       \n";
  std::cout << " Cargando el Modulo :                                                          \n";
@@ -49,11 +52,27 @@ void DisplaceModifier::ShowHelp() const
 
 std::string DisplaceModifier::Keywords() const
 {
- return "x y z start end each";
+ return "x y z autocenter start end each";
 }
 
 void DisplaceModifier::Apply(SimulationCell & sc)
 {
+ if (GetString("autocenter") == "true")
+ {
+  Vector cm(0.0, 0.0, 0.0);
+  Vector boxcenter(0.5, 0.5, 0.5);
+  sc.ScaleByCell(boxcenter);
+  double tm = 0.0;
+  for (long int i=0;i<sc.Size();++i)
+  {
+   const Atom & at = sc[i];
+   cm += at.Mass()*at.Position();
+   tm += at.Mass();
+  }
+  cm = cm*(1.0/tm);
+  offset = boxcenter - cm;
+  std::cerr << "-> Autocentering atoms: offset = " << offset << '\n';
+ }
  for (long int i=0;i<sc.Size();++i)
  {
   Vector pos = sc.GetAtom(i).Position() + offset; 
