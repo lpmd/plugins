ae917f643ba09568394076a604c6235d55302167
sdavis <sdavis@d2ad0ced-163c-4466-90a1-0dc60eb45a10>
2009-06-03 00:10:13 +0000

    - Nuevo plugin 'monitor' es el visualizador que corre mostrando los pasos, energias, etc. - Es mas general que el antiguo monitor, ya que puede mostrar cualquier Tag de la simulacion actual

---
+++ plugins/testing/monitor.cc
@@ -0,0 +1,60 @@
+//
+//
+//
+
+#include "monitor.h"
+
+#include <lpmd/simulation.h>
+
+#include <iostream>
+#include <fstream>
+
+using namespace lpmd;
+
+MonitorVisualizer::MonitorVisualizer(std::string args): Module("monitor")
+{
+ ParamList & params = (*this);
+ AssignParameter("version", "1.0"); 
+ AssignParameter("apirequired", "1.1"); 
+ AssignParameter("bugreport", "gnm@gnm.cl"); 
+ //
+ DefineKeyword("start");
+ DefineKeyword("end");
+ DefineKeyword("each");
+ DefineKeyword("properties", "step");
+ DefineKeyword("output", "-");
+ ProcessArguments(args);
+ start = int(params["start"]);
+ end = int(params["end"]);
+ each = int(params["each"]);
+ if (params["output"] != "-") output_stream = new std::ofstream(params["output"].c_str());
+ else output_stream = &(std::cout);
+ property_array = StringSplit(params["properties"], ',');
+}
+
+MonitorVisualizer::~MonitorVisualizer()
+{ 
+ ParamList & params = (*this);
+ if (params["output"] != "-") delete output_stream;
+}
+
+void MonitorVisualizer::ShowHelp() const
+{
+ std::cout << " General Info      >>                                                          \n";
+ std::cout << " General Options   >>                                                          \n";
+ std::cout << '\n';
+ std::cout << " Example                                                                       \n";
+ std::cout << " monitor step,temperature start=1 end=1000 each=50                           \n\n";
+}
+
+void MonitorVisualizer::Apply(const Simulation & sim)
+{ 
+ for (int p=0;p<property_array.Size();++p) 
+     (*output_stream) << sim.GetTag(sim, Tag(property_array[p])) << "  ";
+ (*output_stream) << '\n';
+}
+
+// Esto se incluye para que el modulo pueda ser cargado dinamicamente
+Module * create(std::string args) { return new MonitorVisualizer(args); }
+void destroy(Module * m) { delete m; }
+
