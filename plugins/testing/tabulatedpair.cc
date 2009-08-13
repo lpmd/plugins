9fab5e75000d949ec81d9f685ae2de001b3c8408
jperalta <jperalta@d2ad0ced-163c-4466-90a1-0dc60eb45a10>
2009-08-13 18:49:39 +0000

    Tabulatedpair no requiere cutoff y son 3 columnas, r - U(r) - F (r)

--- plugins/testing/tabulatedpair.cc
+++ plugins/testing/tabulatedpair.cc
@@ -14,13 +14,10 @@ TabulatedPair::TabulatedPair(std::string args): Plugin("fastlj", "2.0")
  ParamList & params = (*this);
  //
  DefineKeyword("file");
- DefineKeyword("cutoff");
- DefineKeyword("bins","500");
  // hasta aqui los valores por omision
  ProcessArguments(args);
- cutoff = params["cutoff"];
- SetCutoff(cutoff);
- bins = int(params["bins"]);
+ cutoff = 0.0e0;
+ bins = 0;
  file = params["file"];
  ReadTable();
 }
@@ -37,14 +34,12 @@ void TabulatedPair::ShowHelp() const
  std::cout << "      El modulo lee un potencial tabulado de pares a partir de un fichero      \n";
  std::cout << " de datos.                                                                     \n";
  std::cout << " General Options   >>                                                          \n";
- std::cout << "      cutoff         : Especifica el valor de radio de corte para el potencial.\n";
  std::cout << "      file           : Especifica el nombre del archivo.                       \n";
  std::cout << '\n';
  std::cout << " Example                                                                       \n";
  std::cout << " Cargando el Modulo :                                                          \n";
  std::cout << " use tabulatedpair as TABLJ                                                    \n";
  std::cout << "     file lennardjones.dat                                                     \n";
- std::cout << "     cutoff 1.90                                                               \n";
  std::cout << " enduse                                                                        \n";
  std::cout << " Llamando al modulo                                                            \n";
  std::cout << " potential TABLJ Ar Ar                                                       \n\n";
@@ -71,7 +66,7 @@ void TabulatedPair::ReadTable()
   RemoveUnnecessarySpaces(line);
   lpmd::Array<std::string> words = StringSplit(line,' ');
   DebugStream() << " -> line = " << line <<" size = " << words.Size() << '\n';
-  if(line[0]!='#' && words.Size()==2) n++;
+  if(line[0]!='#' && words.Size()==3) n++;
  }
  input.clear();input.seekg(0);
  DebugStream() << "-> Readed "<<n<<" lines " << '\n';
@@ -79,19 +74,22 @@ void TabulatedPair::ReadTable()
  etable = new double[bins];
  ftable = new double[bins];
  int i=0;
+ double previo = 0.0e0;
  //Asign values
  while(!input.eof())
  {
   getline(input,line);
   RemoveUnnecessarySpaces(line);
   lpmd::Array<std::string> words = StringSplit(line,' ');
-  if(words.Size()==2 && line[0]!='#')
+  if(words.Size()==3 && line[0]!='#')
   {
-   etable[i] = atof(words[0].c_str());
-   ftable[i] = atof(words[1].c_str());
+   if(atof(words[0].c_str())>previo) { cutoff = atof(words[0].c_str()); previo = cutoff;}
+   etable[i] = atof(words[1].c_str());
+   ftable[i] = atof(words[2].c_str());
    i++;
   }
  }
+ SetCutoff(cutoff);
  DebugStream() << "-> Table was readed correctly." << '\n';
 }
 
