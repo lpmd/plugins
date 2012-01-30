3d5d63369b3a26b0100a5d3094baa472bf98f4e7
fgonzalez <fgonzalez@d2ad0ced-163c-4466-90a1-0dc60eb45a10>
2012-01-30 08:43:27 +0000

    Primera version mejorada del plugin 'vasp'. Ahora lee velocidades en los POSCAR y lee XDATCAR.

--- vasp.cc
+++ vasp.cc
@@ -11,24 +11,30 @@
 
 using namespace lpmd;
 
-VaspFormat::VaspFormat(std::string args): Plugin("vasp", "2.1")
+VaspFormat::VaspFormat(std::string args): Plugin("vasp", "3.1")
 {
  ParamList & params = (*this);
  //
  DefineKeyword("file");
  DefineKeyword("species", "NULL");
+ DefineKeyword("numatoms", "NULL");
  DefineKeyword("each", "1");
  DefineKeyword("level", "0");
  DefineKeyword("type", "Direct");
+ DefineKeyword("ftype", "poscar");
  AssignParameter("replacecell", "false");
  ProcessArguments(args);
  readfile = writefile = params["file"];
  interval = int(params["each"]);
  level = int(params["level"]);
  speclist = params["species"];
+ numatoms = params["numatoms"];
  satoms = StringSplit(speclist,',');
+ numesp = StringSplit(numatoms,',');
  tp = params["type"];
+ ftype = params["ftype"];
  rcell = params["replacecell"];
+ first=true;
 }
 
 VaspFormat::~VaspFormat() { }
@@ -47,78 +53,114 @@ void VaspFormat::ShowHelp() const
  std::cout << "      file          : Input/output file that contains the atomic configurations\n";
  std::cout << "                      in POSCAR format.                                        \n";
  std::cout << "      species       : Atomic species list (in order) of the POSCAR file.       \n";
+ std::cout << "      numatoms      : Number of atoms per species.                             \n";
  std::cout << "      each          : Determines how often (each how many time-steps) the      \n";
  std::cout << "                      input/output file must be read/written.                  \n";
  std::cout << "      level         : Determines the file format level (0/1/2).                \n";
  std::cout << "                      0/1/2 <-> pos/pos-vel/pos-vel-acel.                      \n";
- std::cout << "      type          : Lattice type (Direct / Cartesian) of the POSCAR file.    \n";
+ std::cout << "      type          : Positions type (Direct / Cartesian) for the              \n";
+ std::cout << "                      POSCAR/CONTCAR file.                                     \n";
+ std::cout << "      ftype         : File type that is going to be read/written               \n";
+ std::cout << "                      (poscar/contcar/xdatcar):                                \n";
+ std::cout << "                      poscar: To read/write POSCAR files. They contain the     \n";
+ std::cout << "                              positions of the ions and their velocities       \n";
+ std::cout << "                              (if there are any).                              \n";
+ std::cout << "                      contcar: To read/write CONTCAR files. They contain the   \n";
+ std::cout << "                               same as the POSCAR file, but an extra block     \n";
+ std::cout << "                               containing the predictor-corrector coordinates  \n";
+ std::cout << "                               needed as an input for the next MD-job in VASP. \n";
+ std::cout << "                               LPMD will skip this block when reading, and will\n";
+ std::cout << "                               not print it when writing.                      \n";
+ std::cout << "                      xdatcar: To read/write XDATCAR files. They contain the   \n";
+ std::cout << "                               position of the ions for every ionic-step. Be   \n";
+ std::cout << "                               aware that XDATCAR files DO NOT contain cell    \n";
+ std::cout << "                               vectors, even when they change.                 \n";
  std::cout << "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n";
  std::cout << " Example           >>                                                          \n";
  std::cout << " #Loading the plugin :                                                         \n";
  std::cout << " input module=vasp file=POSCAR species=Si level=0                              \n";
- std::cout << " output module=vasp file=POSCAR.output species=Si level=1 each=5             \n\n";
- std::cout << "      In this way we can read and write atomic configurations in VASP          \n";
- std::cout << "      formats (POSCAR). The file extension (.output) is irrelevant, what       \n";
- std::cout << "      matters is the module loaded (module=vasp).                              \n";
+ std::cout << " output module=vasp file=XDATCAR-01.output species=Si,O ftype=xdatcar numatoms=16,32\n\n";
+ std::cout << "      This plugin is used to read and write configurations in VASP's formats   \n";
+ std::cout << "      (POSCAR, CONTCAR and XDATCAR). The file extension (.output) is irrelevant,\n";
+ std::cout << "      what matters is the module loaded (module=vasp).                         \n";
  std::cout << "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n";
 }
 
-void VaspFormat::ReadHeader(std::istream & is) const { assert(&is != 0);}//icc 869
+void VaspFormat::ReadHeader(std::istream & is) const {assert(&is != 0);}//icc 869
 
 // 
-// Lee una configuracion desde un archivo CONFIG 
+// Reads file from POSCAR/CONTCAR/XDATCAR
 //
 bool VaspFormat::ReadCell(std::istream & is, Configuration & con) const
 {
  BasicParticleSet & part = con.Atoms();
  BasicCell & cell = con.Cell();
  assert(part.Size()==0);
-
  if(speclist=="NULL") throw PluginError("vasp", "Error, you must pass a species list.");
  double scale=1.0e0;
  std::string tmp;
  Vector cv[3];
  double x, y, z;
- getline(is, tmp);           // lee la linea de titulo
- if (is.eof()) return false; // no hay mas configuraciones que leer
- //lee factor de escala
- getline(is, tmp);
- std::istringstream ost(tmp);
- ost >> scale;
- //lee vectores base cv[]
- for (int i=0;i<3;++i)
+ if (first) getline(is,tmp);  // Read title/atoms for POSCAR/XDATCAR.
+ if (is.eof()) return false; // no more configurations to read
+ 
+ // Read Cell Vectors for POSCAR/CONTCAR files
+ if (ftype=="poscar" || ftype=="contcar")
  {
-  getline(is, tmp);
-  std::istringstream vst(tmp);
-  vst >> x >> y >> z;
-  cv[i] = scale*Vector(x, y, z);
-  if ((*this)["replacecell"] == "true") cell[i] = cv[i];
- } 
- //lee y chequea especies atomicas.
- getline(is,tmp);
- RemoveUnnecessarySpaces(tmp);
- Array<std::string> numesp = StringSplit(tmp,' ');
+  getline(is, tmp);           // read scale factor
+  std::istringstream ost(tmp);
+  ost >> scale;
+  //Read cell vectors, cv[]
+  for (int i=0;i<3;++i)
+  {
+   getline(is, tmp);
+   std::istringstream vst(tmp);
+   vst >> x >> y >> z;
+   cv[i] = scale*Vector(x, y, z);
+   if ((*this)["replacecell"] == "true") cell[i] = cv[i];
+  }
+ }
 
- //lee si la red es directa o cartesiana, el caso de selective dynamics lo ignora.
- getline(is,tmp);
+ // For POSCAR/CONTCAR: Read and check out the amount of atoms for each species
+ if (ftype=="poscar" || ftype=="contcar")  getline(is, tmp);
  RemoveUnnecessarySpaces(tmp);
+ if (first && numatoms=="NULL") numesp = StringSplit(tmp,' ');
+
+ // Reads the type of atomic positions (Direct/Cartesian). In the case of 'selective dynamics', ignore it.
  std::string tipo="Direct";
- if(tmp[0]=='s' || tmp[0]=='S') getline(is,tmp);
-
- if(tmp[0]=='c' || tmp[0]=='C') 
+ if (ftype=="poscar" || ftype=="contcar")
  {
-  tipo="Cartesian";
+  getline(is,tmp);
+  RemoveUnnecessarySpaces(tmp);
+  if(tmp[0]=='s' || tmp[0]=='S') getline(is,tmp);
+ 
+  if(tmp[0]=='c' || tmp[0]=='C') 
+  {
+   tipo="Cartesian";
+  }
+  else if(tmp[0]=='d' || tmp[0]=='D')
+  {
+   tipo="Direct";
+  }
+  else ShowWarning("plugin vasp", "The type of cell couldn't be read correctly, type=\"Direct\" was assumed.");
  }
- else if(tmp[0]=='d' || tmp[0]=='D')
+ else if (ftype=="xdatcar" && first)
  {
-  tipo="Direct";
+  for (int i=0; i<5; ++i) {getline(is,tmp); first=false;}// skip unnecesary lines
  }
- else ShowWarning("plugin vasp", "The type of cell couldn't be read correctly, type=\"Direct\" was assumed.");
 
- if (numesp.Size()!=satoms.Size()) { throw PluginError("vasp", "Error, the species number and file POSCAR are different."); }
+ // Check that the amount of atoms match.
+ bool aff = numesp.Size()!=satoms.Size(); // For poscar and contcar, check that N("Si,O")=N("16,32")=2, for example.
+ if (ftype=="xdatcar") aff=false;
+
+ if (aff)
+ {
+  throw PluginError("vasp", "Error, the number of species given does not match with the number of species of the POSCAR file.");
+ }
  else 
  {
   long int S=numesp.Size();
+  if (ftype=="xdatcar" && numatoms=="NULL") S=1; // Assume one species if none is given.
   for(int i=0;i<S;++i)
   {
    std::string symbol=satoms[i];
@@ -126,6 +168,7 @@ bool VaspFormat::ReadCell(std::istream & is, Configuration & con) const
    for(int j=0;j<ns;++j)
    {
     getline(is,tmp);
+    RemoveUnnecessarySpaces(tmp);
     std::istringstream vst(tmp);
     vst >> x >> y >> z;
     Vector vtmp = Vector(x, y, z);
@@ -146,8 +189,46 @@ bool VaspFormat::ReadCell(std::istream & is, Configuration & con) const
     part.Append(this_atom);
    }
   }
+
+
+  // Read velocities, if there are.
+  if (ftype=="poscar" || ftype=="contcar")
+  {
+   getline(is,tmp);
+   getline(is,tmp);
+   RemoveUnnecessarySpaces(tmp);
+
+   if (is.eof()) return false; // no more configurations to read
+   while (tmp=="" && !is.eof()) {getline(is,tmp); RemoveUnnecessarySpaces(tmp);}
+   
+   if (tmp!="")
+   {
+    int n=0;
+    for (int i=0; i<numesp.Size(); ++i) n+=(int)atof(numesp[i].c_str());
+    for(int j=0;j<n;++j)
+    {
+     if (is.eof())
+     {
+      throw PluginError("vasp", "Error, the number of velocities does not match with the number of atoms.");
+      return false;
+     }
+     std::istringstream vst(tmp);
+     vst >> x >> y >> z;
+     Vector vtmp = Vector(x, y, z);
+     part[j].Velocity() = vtmp;
+     getline(is,tmp);
+    }
+    // Skip everything below the velocities
+    while (!is.eof()) getline(is,tmp); 
+   }
+  }
+  else if (ftype=="xdatcar")
+  {
+   getline(is,tmp);  // Read the blank space below the block and throw an error if it's not blank
+   if (!first && StringSplit(tmp,' ').Size()>2) throw PluginError("vasp", "Error, numatoms does not match with the actual number of atoms in the file.");
+  }
  }
- con.SetTag(con, Tag("level"), 0);
+ con.SetTag(con, Tag("level"), 1);
  return true;
 }
 
@@ -162,15 +243,15 @@ void VaspFormat::WriteCell(std::ostream & out, Configuration & con) const
  BasicParticleSet & part = con.Atoms();
  BasicCell & cell = con.Cell();
 
- out << "Generated by LPMD vasp plugin\n";
- out << "  1  " << '\n'; 
+ out << "Generated by LPMD's vasp plugin\n";
+ out << "   1.0" << '\n'; 
  for (int i=0;i<3;++i)
  {
   out.setf(std::ios::left);
   out.setf(std::ios::fixed);
-  out << " " << std::setw(8) << std::setprecision(8) << cell[i][0]; 
-  out << " " << std::setw(8) << std::setprecision(8) << cell[i][1]; 
-  out << " " << std::setw(8) << std::setprecision(8) << cell[i][2]; 
+  out << "     " << std::setw(8) << std::setprecision(8) << cell[i][0]; 
+  out << "     " << std::setw(8) << std::setprecision(8) << cell[i][1]; 
+  out << "     " << std::setw(8) << std::setprecision(8) << cell[i][2]; 
   out << '\n';
  }
  Array<int> esp=part.Elements();
@@ -202,10 +283,24 @@ void VaspFormat::WriteCell(std::ostream & out, Configuration & con) const
     double ly = cell[1].Module();
     double lz = cell[2].Module();
     tmp = Vector(tmp[0]/lx,tmp[1]/ly,tmp[2]/lz);
-    out << tmp << '\n';
+    out << "  " << tmp << '\n';
    }
   }
  }
+ 
+ // Print velocities, if there are.
+ if (level==1)
+ {
+  out << '\n';
+  for(long i=0 ; i < list.Size() ; ++i)
+  {
+   for (long int j=0;j<list[i].Size();++j)
+   {
+    out << "  " << part[list[i][j]].Velocity() << '\n';
+   }
+  }
+ }
+
 }
 
 // Esto se incluye para que el modulo pueda ser cargado dinamicamente
--- vasp.h
+++ vasp.h
@@ -16,20 +16,25 @@ class VaspFormat: public lpmd::CellFormat, public lpmd::Plugin
   virtual ~VaspFormat();
   void ShowHelp() const;
 
-  //Metodos propios de modulo dlpoly
+  //Metodos propios de modulo vasp
   void ReadHeader(std::istream & is) const;
   bool ReadCell(std::istream & is, lpmd::Configuration & con) const;
   void WriteHeader(std::ostream & os, lpmd::SimulationHistory * sh) const;
   void WriteCell(std::ostream & os, lpmd::Configuration & con) const;
   long int GetInterval() const { return interval; }
 
- public:
+ private:
   long int interval;
   int level;
   bool rcell;
+  mutable bool first;
   std::string speclist;
-  lpmd::Array<std::string> satoms; //simbolos atomicos
-  std::string tp; // tipo de celda Direct/Cartesian
+  std::string numatoms;
+  lpmd::Array<std::string> satoms; // atomic symbols
+  lpmd::Array<std::string> nesp; // number of atom per spicies
+  mutable lpmd::Array<std::string> numesp; // number of atom per spicies
+  std::string tp;  // formato posiciones (Direct/Cartesian)
+  std::string ftype; // file type (POSCAR, CONTCAR, XDATCAR)
 };
 
 #endif
