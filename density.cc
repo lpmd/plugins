3fde69b250edb18f7e00b272d2c57af627c046b9
jperaltac <jperaltac@d2ad0ced-163c-4466-90a1-0dc60eb45a10>
2012-06-20 16:42:26 +0000

    Se elimina randomatom del repositorio. Su funcionalidad se puede llevar a cabo por dos plugins (revisar e-mail enviado June 18 - 2012 ). El plugin density acepta el flag info para mostrar la densidad de una celda. Falta la posibilidad de hacer reemplazo de especies, pero no parece fundamental por ahora.

--- density.cc
+++ density.cc
@@ -35,6 +35,7 @@ void DensityModifier::ShowHelp() const
  std::cout << "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n";
  std::cout << " General Options   >>                                                          \n";
  std::cout << "      type          : Type of cell axis modification. The availables are:      \n";
+ std::cout << "                      info  - Only show information about the density.         \n";
  std::cout << "                      hydro - Hydrostatic modification (all axis).             \n";
  std::cout << "                      x,y,z - modify only one axis of the cell.                \n";
  std::cout << "                      xy,yz,xz - modify the two axis only.                     \n";
@@ -52,7 +53,7 @@ void DensityModifier::Apply(Simulation & conf)
  const double df = double(GlobalSession["ua3togrcm3"]);
  BasicParticleSet & atoms = conf.Atoms();
  BasicCell & cell = conf.Cell();
- if(density<=0) throw PluginError("density","Error in density value, must be positive and non-zero.");
+ if(density<=0 && type!="info") throw PluginError("density","Error in density value, must be positive and non-zero.");
 
  double mass = 0.0e0;
  for(int i=0;i<atoms.Size();++i)
@@ -60,9 +61,13 @@ void DensityModifier::Apply(Simulation & conf)
   mass += atoms[i].Mass();
  }
  double dens = df*mass/cell.Volume();
- std::cout << "\n\nOriginal density = " << dens << '\n';
+ if(type == "info")
+ {
+  std::cout << "\n\nOriginal density = " << dens << "gr/cm^3" << '\n';
+  std::cout << "Number of atoms = " << atoms.Size() << '\n';
+ }
  //Change the density of the cell.
- if (type == "hydro")
+ else if (type == "hydro")
  {
   while(fabs(density-dens)>1E-2)
   {
